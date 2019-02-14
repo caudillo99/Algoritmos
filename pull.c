4dbcde9730435aaacb0cbacf17378d9b1487a612
caudillo99 <diego.caudillo@uabc.edu.mx>
2019-02-13 21:06:49 -0800

    update pull.c

--- pull.c
+++ pull.c
@@ -1,9 +1,49 @@
 #include <stdio.h>
+typedef  struct
+{
+	char name[10];
+	char last_name[15];
+	int age;
+}datos_t;
 
+//Headers
+void captura(datos_t *persona);
+void printData(datos_t persona);
+void createPush(void);
 int main ()
 {
-  // Este un archivo ejemplo para realizar un pull desde terminal
-  
-  printf("Pull realizado con exito!!!");
-  return 0;
+
+	datos_t persona;
+	captura(&persona);
+	printData(persona);
+	createPush(); 
+  	return 0;
 }
+
+// funcion que captura datos de una estructura de datos
+void captura(datos_t* persona)
+{
+	printf("Name: "); gets(persona->name);
+	printf("Lasta name: "); gets (persona->last_name);
+	printf("Age: "); scanf("%d", &persona->age);
+	system("clear");
+}
+
+void printData(datos_t persona)
+{
+	printf("Name: %s\n", persona.name);
+	printf("Last name: %s\n", persona.last_name);
+	printf("Age: %d\n",persona.age); 
+}
+
+void createPush(void)
+{
+	char str[] = "Succesful";
+	system("git status");
+	system("git commit -m 'making changes'");
+	system("git add .");
+	system("git push origin master");
+	system("echo %s",str);
+}
+
+
